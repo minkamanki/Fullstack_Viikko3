ccf39d2405abb563aeac80829b4c165bb1048205
minkamanki <minka.tahvanainen@gmail.com>
2021-09-11 14:24:31 +0300

    Viikko 3 tehtävät 3.1-3.8

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+var morgan = require('morgan')
+app.use(express.json())
+//app.use(morgan('combined'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms :body'));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const length = persons.length
+    const date = new Date()
+    res.send(`Phonebook has info for ${length} people <div>${date}</div>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    if (!person.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!person.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (persons.some((per) => (per.name === person.name))) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    person.id = Math.floor(Math.random() * 1000)
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
